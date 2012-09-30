8c449e81acc9d8b238b61273a65d2ed498061d92
Stuart Caborn <stooferus@gmail.com>
2012-09-30 12:06:45 +0100

    Don't repeatedly create path entries

--- kitchen/cookbooks/jdk/recipes/default.rb
+++ kitchen/cookbooks/jdk/recipes/default.rb
@@ -9,18 +9,18 @@
 
 java_home_dir = "C:\\java\\jdk7-64bit"
 
-env "JAVA_HOME" do
-  value "#{java_home_dir}\\bin"
-  action :create
-end
-  
-windows_path "%JAVA_HOME%" do
-  action :add
-end
-
 if
   begin 
     !File.exists?("#{java_home_dir}\\bin\\javac.exe")
+  end
+  
+  env "JAVA_HOME" do
+    value "#{java_home_dir}\\bin"
+    action :create
+  end
+  
+  windows_path "%JAVA_HOME%" do
+    action :add
   end
   
   cookbook_file "c:\\temp\\jdk-7u7-windows-x64.exe" do
